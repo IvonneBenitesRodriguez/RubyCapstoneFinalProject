b75eace162a2da9efc75fc3aa856f5a60c9cdfe1
gilberthappi <gdushimimana6@gmail.com>
2023-11-30 23:26:41 +0200

    Adding files for a

--- database/schema.sql
+++ database/schema.sql
@@ -26,3 +26,36 @@ CREATE TABLE book(
     item_id INTEGER,
     FOREIGN KEY(item_id) REFERENCES items(id)
 );
+
+
+CREATE TABLE author(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+);
+
+CREATE TABLE game(
+    id INTEGER PRIMARY KEY,
+    multiplayer BOOLEAN,
+    last_played_at DATE,
+    item_id INTEGER,
+    FOREIGN KEY(item_id) REFERENCES items(id)
+);
+
+CREATE TABLE genre (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200)
+);
+
+CREATE TABLE music_album (
+    id INTEGER PRIMARY KEY,
+    on_spotify BOOLEAN,
+    publish_date DATE,
+    archived BOOLEAN,
+    genre_id INTEGER,
+    FOREIGN KEY (genre_id) REFERENCES genre(id),
+    genre_name VARCHAR(200)
+);
+
+CREATE INDEX idx_book_item_id ON book(item_id);
+CREATE INDEX idx_item_label_id ON item(label_id);
